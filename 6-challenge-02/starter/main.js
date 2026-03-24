565ddba26c507e4bb5ca14e88db482e97cc795c7
guozhao709 <guozhao709@qq.com>
2026-03-24 20:31:24 +0800

    vite

--- 6-challenge-02/starter/main.js
+++ 6-challenge-02/starter/main.js
@@ -4,36 +4,100 @@
 */
 
 function MyApp() {
+  // 制作列表响应式数据
   const [todoList, setTodoList] = React.useState([
     {
       id: 0,
       item: "Learn React",
+      checked: false,
     },
     {
       id: 1,
       item: "learn vue",
+      checked: false,
     },
     {
       id: 2,
       item: "learn angular",
+      checked: false,
     },
   ]);
 
+  // 输入框响应式数据
+  const [inputValue, setInputValue] = React.useState("");
+
+  // 删去所选数据
   const handleDelete = (list) => {
     const newTodoList = todoList.filter((item) => item.id !== list.id);
+    setTodoList([...newTodoList]);
+  };
+
+
+  const handleAdd = () => {
+    let max = 0;
+    todoList.forEach((item) => {
+      item.id > max ? (max = item.id) : max;
+    });
+
+    setTodoList([...todoList, { id: max + 1, item: inputValue }]);
+    // 不要忘记重新设置输入框
+    setInputValue("");
+  };
+
+  const handleChecked = (list) => {
+    const newTodoList = todoList.map((item) => {
+      if (item.id === list.id) {
+        return { ...item, checked: !item.checked };
+      }
+      return item;
+    });
+    // setTodoList([...newTodoList]);
+
+    // const newTodoList = todoList.filter((item) => {
+    //   return item.id !== list.id;
+    // });
+    // newTodoList.push({
+    //   ...list,
+    //   checked: !list.checked,
+    // });
+    // setTodoList(newTodoList);
+
+    // 排序, 正数表示往后排, 负数表示往前排
+    newTodoList.sort((a, b) => {
+      if(a.checked && !b.checked){
+        return 1;
+      }
+      if(!a.checked && b.checked){
+        return -1;
+      }
+      return 0;
+    });
+
     setTodoList([...newTodoList]);
   };
 
   return (
     <main>
       <h1>React Todo List</h1>
-      <input type="text" placeholder="Add item into as todo" />
-      <button>Add</button>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        type="text"
+        placeholder="Add item into as todo"
+      />
+      <button onClick={() => handleAdd()}>Add</button>
       <ul>
         {todoList.map((list) => (
           <li key={list.id}>
-            <input type="checkbox" />
-            <span>{list.item}</span>
+            <input
+              type="checkbox"
+              checked={list.checked}
+              onChange={() => handleChecked(list)}
+            />
+            <span className={`${list.checked ? "deleted" : ""}`}>
+              {list.item}
+            </span>
+            {/* 删除按钮点击事件, 注意传的是函数, 而不是调用() */}
             <button onClick={() => handleDelete(list)}>delete</button>
           </li>
         ))}
